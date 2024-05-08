bc07723b03eef172a8c9cd84f1478cea616c9482
Maksym Bezkorovainyi <maksymus@users.noreply.github.com>
2024-05-08 11:51:31 -0400

    Create worker.go

---
+++ patterns/worker.go
@@ -0,0 +1,47 @@
+package patterns
+
+import (
+    "sync"
+)
+
+func Work(nums []int, numWorkers int, f func(int) int) (ret []int) {
+    in := make(chan int, numWorkers)
+    out := make(chan int, numWorkers)
+
+    var wg sync.WaitGroup
+
+    // start workers
+    for i := 0; i < numWorkers; i++ {
+        wg.Add(1)
+        go work(in, out, &wg, f)
+    }
+    
+    go func() {
+        wg.Wait()
+        close(out)
+    }()
+
+    // generate data
+    go func() {
+        defer close(in)
+
+        for _, num := range nums {
+            in <- num
+        }
+    }()
+
+    // process result
+    for res := range out {
+       ret = append(ret, res)
+    }
+
+    return ret
+}
+
+func work(in <-chan int, out chan<- int, wg *sync.WaitGroup, f func(int) int) {
+    defer wg.Done()
+
+    for num := range in {
+        out <- f(num)
+    }
+}
